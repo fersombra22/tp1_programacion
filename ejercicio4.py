c1e304370e7b3a4d6f2517f7424060ecc9b74cc8
fernando sombra <fersombra10@gmail.com>
2024-05-11 14:28:04 -0300

    secuencial y condicional 1

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,4 +1,4 @@
 print("ingrese un numero en fomato texto")
 palabra=input()
 num=int(palabra)
-print(num)
+print(f" la conversion es: {num}  como entero. ")
---
+++ ejercicio5.py
@@ -0,0 +1,8 @@
+#5.	EscribaunprogramaenPython,sabiendoqueunamillaequivalea1,
+#609kilómetros,leaunacantidadenmillasy laconvierteaKilómetros.
+print("ingrese la cantidad de millas a convertir")
+millas=float(input())
+
+km = millas / 1609
+
+print(f"{millas} millas equivalen a {int(km)}  kilómetros.") 
